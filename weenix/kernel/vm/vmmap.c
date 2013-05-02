a9809f614c9533c55d14dfafb5b19e4160a1b55a
walnutown <walnutown@gmail.com>
2013-05-02 05:19:00 -0700

    bdg color

--- weenix/kernel/vm/vmmap.c
+++ weenix/kernel/vm/vmmap.c
@@ -39,22 +39,22 @@ vmmap_init(void)
 vmarea_t *
 vmarea_alloc(void)
 {
-        dbg(DBG_VM,"VM: Enter vmarea_alloc()");
+        dbg(DBG_VFS,"VM: Enter vmarea_alloc()\n");
         vmarea_t *newvma = (vmarea_t *) slab_obj_alloc(vmarea_allocator);
         if (newvma) {
                 newvma->vma_vmmap = NULL;
         }
-        dbg(DBG_VM,"VM: Leave vmarea_alloc()");
+        dbg(DBG_VFS,"VM: Leave vmarea_alloc()");
         return newvma;
 }
 
 void
 vmarea_free(vmarea_t *vma)
 {
-        dbg(DBG_VM,"VM: Enter vmarea_free()");
+        dbg(DBG_VFS,"VM: Enter vmarea_free()\n");
         KASSERT(NULL != vma);
         slab_obj_free(vmarea_allocator, vma);
-        dbg(DBG_VM,"VM: Leave vmarea_free()");
+        dbg(DBG_VFS,"VM: Leave vmarea_free()\n");
 }
 
 /* Create a new vmmap, which has no vmareas and does
@@ -63,13 +63,13 @@ vmarea_free(vmarea_t *vma)
 vmmap_t *
 vmmap_create(void)
 {
-        dbg(DBG_VM,"VM: Enter vmmap_create()");
+        dbg(DBG_VFS,"VM: Enter vmmap_create()\n");
         vmmap_t *newvmm = (vmmap_t *)slab_obj_alloc(vmmap_allocator);
         if(newvmm) {
                 list_init(&newvmm->vmm_list);
                 newvmm->vmm_proc = NULL;
         }
-        dbg(DBG_VM,"VM: Leave vmmap_create()");
+        dbg(DBG_VFS,"VM: Leave vmmap_create()\n");
         return newvmm;
 
         /*
@@ -84,7 +84,7 @@ vmmap_create(void)
 void
 vmmap_destroy(vmmap_t *map)
 {
-        dbg(DBG_VM,"VM: Enter vmmap_destroy()");
+        dbg(DBG_VFS,"VM: Enter vmmap_destroy()\n");
         KASSERT(NULL != map);
 
         if(!list_empty(&map->vmm_list)) {
@@ -96,7 +96,7 @@ vmmap_destroy(vmmap_t *map)
         }
         map->vmm_proc=NULL;
         slab_obj_free(vmmap_allocator, map);
-        dbg(DBG_VM,"VM: Leave vmmap_destroy()");
+        dbg(DBG_VFS,"VM: Leave vmmap_destroy()");
         /*
         NOT_YET_IMPLEMENTED("VM: vmmap_destroy");
         */
@@ -110,7 +110,7 @@ vmmap_destroy(vmmap_t *map)
 void
 vmmap_insert(vmmap_t *map, vmarea_t *newvma)
 {
-        dbg(DBG_VM,"VM: Enter vmmap_insert()");
+        dbg(DBG_VFS,"VM: Enter vmmap_insert()\n");
         KASSERT(NULL != map && NULL != newvma);
         KASSERT(NULL == newvma->vma_vmmap);
         KASSERT(newvma->vma_start < newvma->vma_end);
@@ -132,14 +132,14 @@ vmmap_insert(vmmap_t *map, vmarea_t *newvma)
                                         newvma->vma_vmmap = map;
                                         /*** need collapse? ***/
                                         list_insert_before((&iterator->vma_plink)->l_next, &newvma->vma_plink);
-                                        dbg(DBG_VM,"VM: Leave vmmap_destroy()");
+                                        dbg(DBG_VFS,"VM: Leave vmmap_destroy()\n");
                                         return ;
                                 }
                                 else 
                                 {
                                         /*** how to deal with overlap? ***/
                                         /*** no overlap here! ***/
-                                        dbg(DBG_VM,"VM: Leave vmmap_destroy(), overlap!!!");
+                                        dbg(DBG_VFS,"VM: Leave vmmap_destroy(), overlap!!!\n");
                                         return ;
                                 }
                         }
@@ -152,7 +152,7 @@ vmmap_insert(vmmap_t *map, vmarea_t *newvma)
         {
                 newvma->vma_vmmap = map;
                 list_insert_head(&map->vmm_list, &newvma->vma_plink);
-                dbg(DBG_VM,"VM: Leave vmmap_destroy()");
+                dbg(DBG_VFS,"VM: Leave vmmap_destroy()\n");
                 return ;
         }
 
@@ -179,7 +179,7 @@ vmmap_insert(vmmap_t *map, vmarea_t *newvma)
 int
 vmmap_find_range(vmmap_t *map, uint32_t npages, int dir)
 {
-        dbg(DBG_VM,"VM: Enter vmmap_find_range()");
+        dbg(DBG_VFS,"VM: Enter vmmap_find_range()\n");
         KASSERT(NULL != map);
         KASSERT(0 < npages);
 
@@ -215,7 +215,7 @@ vmmap_find_range(vmmap_t *map, uint32_t npages, int dir)
                                 }
                                 if(pages >= npages)
                                 {
-                                        dbg(DBG_VM,"VM: Leave vmmap_find_range()");
+                                        dbg(DBG_VFS,"VM: Leave vmmap_find_range()\n");
                                         return page_no;
                                 }
                         }
@@ -226,7 +226,7 @@ vmmap_find_range(vmmap_t *map, uint32_t npages, int dir)
                                 page_no = vma->vma_end;
                                 if(pages >= npages)
                                 {
-                                        dbg(DBG_VM,"VM: Leave vmmap_find_range()");
+                                        dbg(DBG_VFS,"VM: Leave vmmap_find_range()\n");
                                         return page_no;
                                 }
                         }
@@ -251,7 +251,7 @@ vmmap_find_range(vmmap_t *map, uint32_t npages, int dir)
                                 }
                                 if(pages >= npages)
                                 {
-                                        dbg(DBG_VM,"VM: Leave vmmap_find_range()");
+                                        dbg(DBG_VFS,"VM: Leave vmmap_find_range()\n");
                                         return page_no;
                                 }
                         }
@@ -262,13 +262,13 @@ vmmap_find_range(vmmap_t *map, uint32_t npages, int dir)
                                 page_no = vma->vma_end;
                                 if(pages >= npages)
                                 {
-                                        dbg(DBG_VM,"VM: Leave vmmap_find_range()");
+                                        dbg(DBG_VFS,"VM: Leave vmmap_find_range()\n");
                                         return page_no;
                                 }
                         }
                 }
         }
-        dbg(DBG_VM,"VM: Leave vmmap_find_range()");
+        dbg(DBG_VFS,"VM: Leave vmmap_find_range()\n");
         return -1;
         /*
         NOT_YET_IMPLEMENTED("VM: vmmap_find_range");
@@ -282,7 +282,7 @@ vmmap_find_range(vmmap_t *map, uint32_t npages, int dir)
 vmarea_t *
 vmmap_lookup(vmmap_t *map, uint32_t vfn)
 {
-        dbg(DBG_VM,"VM: Enter vmmap_lookup()");
+        dbg(DBG_VFS,"VM: Enter vmmap_lookup()\n");
         KASSERT(NULL != map);
 
         if(!list_empty(&map->vmm_list)) 
@@ -292,12 +292,12 @@ vmmap_lookup(vmmap_t *map, uint32_t vfn)
                 {              
                         if(vfn >= iterator->vma_start && vfn < iterator->vma_end) 
                         {
-                            dbg(DBG_VM,"VM: Leave vmmap_lookup(), found!");
+                            dbg(DBG_VFS,"VM: Leave vmmap_lookup(), found!");
                             return iterator;
                         }
                 } list_iterate_end();
         }
-        dbg(DBG_VM,"VM: Leave vmmap_lookup(), not found!");
+        dbg(DBG_VFS,"VM: Leave vmmap_lookup(), not found!");
         return NULL;
         /*
         NOT_YET_IMPLEMENTED("VM: vmmap_lookup");
@@ -313,7 +313,7 @@ vmmap_lookup(vmmap_t *map, uint32_t vfn)
 vmmap_t *
 vmmap_clone(vmmap_t *map)
 {
-        dbg(DBG_VM,"VM: Enter vmmap_clone()");
+        dbg(DBG_VFS,"VM: Enter vmmap_clone()\n");
         KASSERT(NULL != map);
 
         /*vmmap_t *clonevmm = (vmmap_t *)slab_obj_alloc(vmmap_allocator);*/
@@ -334,7 +334,7 @@ vmmap_clone(vmmap_t *map)
                 vmmap_insert(clonevmm,newvma);
             }list_iterate_end();
         }
-        dbg(DBG_VM,"VM: Leave vmmap_clone()");
+        dbg(DBG_VFS,"VM: Leave vmmap_clone()\n");
         return clonevmm;
 
         /*
@@ -373,7 +373,7 @@ int
 vmmap_map(vmmap_t *map, vnode_t *file, uint32_t lopage, uint32_t npages,
           int prot, int flags, off_t off, int dir, vmarea_t **new)
 {
-        dbg(DBG_VM,"VM: Enter vmmap_map()");
+        dbg(DBG_VFS,"VM: Enter vmmap_map()\n");
         KASSERT(NULL != map);
         KASSERT(0 < npages);
         KASSERT(!(~(PROT_NONE | PROT_READ | PROT_WRITE | PROT_EXEC) & prot));
@@ -390,13 +390,13 @@ vmmap_map(vmmap_t *map, vnode_t *file, uint32_t lopage, uint32_t npages,
             int vfn_start=vmmap_find_range(map,npages,dir);
             if(vfn_start==-1)
             {
-                dbg(DBG_VM,"VM: Leave vmmap_map(), error");
+                dbg(DBG_VFS,"VM: Leave vmmap_map(), error\n");
                 return -1;
             }
             newvma=vmarea_alloc();
             if(newvma==NULL)
             {
-                dbg(DBG_VM,"VM: Leave vmmap_map(), error");
+                dbg(DBG_VFS,"VM: Leave vmmap_map(), error\n");
                 return -1;
             }
             newvma->vma_start=vfn_start;
@@ -409,7 +409,7 @@ vmmap_map(vmmap_t *map, vnode_t *file, uint32_t lopage, uint32_t npages,
                 err=vmmap_remove(map, lopage, npages);
                 if(err!=0)
                 {
-                    dbg(DBG_VM,"VM: Leave vmmap_map(), error");
+                    dbg(DBG_VFS,"VM: Leave vmmap_map(), error\n");
                     return err;
                 }
             }
@@ -427,7 +427,7 @@ vmmap_map(vmmap_t *map, vnode_t *file, uint32_t lopage, uint32_t npages,
             obj=anon_create();
             if(obj==NULL)
             {
-                dbg(DBG_VM,"VM: Leave vmmap_map(), error");
+                dbg(DBG_VFS,"VM: Leave vmmap_map(), error\n");
                 return -1;
             }
             obj->mmo_ops->ref(obj);
@@ -441,7 +441,7 @@ vmmap_map(vmmap_t *map, vnode_t *file, uint32_t lopage, uint32_t npages,
         {
             if((err=(file->vn_ops->mmap)(file,newvma,&obj))<0)
             {
-                dbg(DBG_VM,"VM: Leave vmmap_map(), error");
+                dbg(DBG_VFS,"VM: Leave vmmap_map(), error\n");
                 return err;
             }
             file->vn_mmobj=*obj;
@@ -454,7 +454,7 @@ vmmap_map(vmmap_t *map, vnode_t *file, uint32_t lopage, uint32_t npages,
         }
         *new=newvma;
         vmmap_insert(map,newvma);
-        dbg(DBG_VM,"VM: Leave vmmap_map()");
+        dbg(DBG_VFS,"VM: Leave vmmap_map()\n");
         return 0;
         /*
         NOT_YET_IMPLEMENTED("VM: vmmap_map");
@@ -494,7 +494,7 @@ vmmap_map(vmmap_t *map, vnode_t *file, uint32_t lopage, uint32_t npages,
 int
 vmmap_remove(vmmap_t *map, uint32_t lopage, uint32_t npages)
 {
-        dbg(DBG_VM,"VM: Enter vmmap_remove()");
+        dbg(DBG_VFS,"VM: Enter vmmap_remove()\n");
         if(!list_empty(&map->vmm_list)) 
         {
                 vmarea_t *iterator;
@@ -509,7 +509,7 @@ vmmap_remove(vmmap_t *map, uint32_t lopage, uint32_t npages)
                                 vmarea_t * newvma2=vmarea_alloc();
                                 if(!newvma1||!newvma2)
                                 {
-                                    dbg(DBG_VM,"VM: Leave vmmap_remove()");
+                                    dbg(DBG_VFS,"VM: Leave vmmap_remove()\n");
                                     return -1;
                                 }
 
@@ -553,7 +553,7 @@ vmmap_remove(vmmap_t *map, uint32_t lopage, uint32_t npages)
 
                 } list_iterate_end();
         }
-        dbg(DBG_VM,"VM: Leave vmmap_remove()");
+        dbg(DBG_VFS,"VM: Leave vmmap_remove()\n");
         return 0;
         /*NOT_YET_IMPLEMENTED("VM: vmmap_remove");
         return -1;*/
@@ -566,7 +566,7 @@ vmmap_remove(vmmap_t *map, uint32_t lopage, uint32_t npages)
 int
 vmmap_is_range_empty(vmmap_t *map, uint32_t startvfn, uint32_t npages)
 {
-        dbg(DBG_VM,"VM: Enter vmmap_is_range_empty()");
+        dbg(DBG_VFS,"VM: Enter vmmap_is_range_empty()\n");
         KASSERT(NULL != map);
         /*KASSERT((startvfn < endvfn) && (ADDR_TO_PN(USER_MEM_LOW) <= startvfn) && (ADDR_TO_PN(USER_MEM_HIGH) >= endvfn));*/
         if(!list_empty(&map->vmm_list)) 
@@ -577,23 +577,23 @@ vmmap_is_range_empty(vmmap_t *map, uint32_t startvfn, uint32_t npages)
                         /* [   *****]**** */
                         if(iterator->vma_start <= startvfn && iterator->vma_end > startvfn) 
                         {
-                                dbg(DBG_VM,"VM: Leave vmmap_is_range_empty()");
+                                dbg(DBG_VFS,"VM: Leave vmmap_is_range_empty()\n");
                                 return 0;
                         } /* ****[****    ] */
                         else if(iterator->vma_start < (startvfn + npages) && iterator->vma_end >= (startvfn + npages)) 
                         {
-                                dbg(DBG_VM,"VM: Leave vmmap_is_range_empty()");
+                                dbg(DBG_VFS,"VM: Leave vmmap_is_range_empty()\n");
                                 return 0;
                         } /* ***[****]*** */
                         else if(iterator->vma_start >= startvfn && iterator->vma_end <= (startvfn + npages)) 
                         {
-                                dbg(DBG_VM,"VM: Leave vmmap_is_range_empty()");
+                                dbg(DBG_VFS,"VM: Leave vmmap_is_range_empty()\n");
                                 return 0;
                         }
 
                 } list_iterate_end();
         }
-        dbg(DBG_VM,"VM: Leave vmmap_is_range_empty()");
+        dbg(DBG_VFS,"VM: Leave vmmap_is_range_empty()\n");
         return 1;
         /*
         NOT_YET_IMPLEMENTED("VM: vmmap_is_range_empty");
@@ -619,7 +619,7 @@ vmmap_is_range_empty(vmmap_t *map, uint32_t startvfn, uint32_t npages)
 int
 vmmap_read(vmmap_t *map, const void *vaddr, void *buf, size_t count)
 {
-        dbg(DBG_VM,"VM: Enter vmmap_read()");
+        dbg(DBG_VFS,"VM: Enter vmmap_read()\n");
         KASSERT(NULL != map);
 
         /*** manipulate vfn ***/
@@ -678,13 +678,13 @@ vmmap_read(vmmap_t *map, const void *vaddr, void *buf, size_t count)
                         }
                         else
                         {
-                                dbg(DBG_VM,"VM: Enter vmmap_read()");
+                                dbg(DBG_VFS,"VM: Enter vmmap_read()\n");
                                 return -EFAULT;
                         }
                 }
                 else
                 {
-                        dbg(DBG_VM,"VM: Enter vmmap_read()");
+                        dbg(DBG_VFS,"VM: Enter vmmap_read()\n");
                         return -EFAULT;
                 } 
         }
@@ -705,17 +705,17 @@ vmmap_read(vmmap_t *map, const void *vaddr, void *buf, size_t count)
                         }
                         else
                         {
-                                dbg(DBG_VM,"VM: Enter vmmap_read()");
+                                dbg(DBG_VFS,"VM: Enter vmmap_read()\n");
                                 return -EFAULT;
                         }
                  }
                  else
                  {
-                        dbg(DBG_VM,"VM: Enter vmmap_read()");
+                        dbg(DBG_VFS,"VM: Enter vmmap_read()\n");
                         return -EFAULT;
                  }
         }
-        dbg(DBG_VM,"VM: Enter vmmap_read()");
+        dbg(DBG_VFS,"VM: Enter vmmap_read()\n");
         return 0;
 
         /*** manipulate address ***/
@@ -804,7 +804,7 @@ vmmap_read(vmmap_t *map, const void *vaddr, void *buf, size_t count)
 int
 vmmap_write(vmmap_t *map, void *vaddr, const void *buf, size_t count)
 {
-        dbg(DBG_VM,"VM: Enter vmmap_write()");
+        dbg(DBG_VFS,"VM: Enter vmmap_write()\n");
         KASSERT(NULL != map);
 
         /*** manipulate vfn ***/
@@ -870,13 +870,13 @@ vmmap_write(vmmap_t *map, void *vaddr, const void *buf, size_t count)
                         }
                         else
                         {
-                                dbg(DBG_VM,"VM: Leave vmmap_write(), EFAULT");
+                                dbg(DBG_VFS,"VM: Leave vmmap_write(), EFAULT\n");
                                 return -EFAULT;
                         }
                 }
                 else
                 {
-                        dbg(DBG_VM,"VM: Leave vmmap_write(), EFAULT");
+                        dbg(DBG_VFS,"VM: Leave vmmap_write(), EFAULT\n");
                         return -EFAULT;
                 } 
         }
@@ -900,17 +900,17 @@ vmmap_write(vmmap_t *map, void *vaddr, const void *buf, size_t count)
                         }
                         else
                         {
-                                dbg(DBG_VM,"VM: Leave vmmap_write(), EFAULT");
+                                dbg(DBG_VFS,"VM: Leave vmmap_write(), EFAULT\n");
                                 return -EFAULT;
                         }
                  }
                  else
                  {
-                        dbg(DBG_VM,"VM: Leave vmmap_write(), EFAULT");
+                        dbg(DBG_VFS,"VM: Leave vmmap_write(), EFAULT\n");
                         return -EFAULT;
                  }
         }
-        dbg(DBG_VM,"VM: Leave vmmap_write(), EFAULT");
+        dbg(DBG_VFS,"VM: Leave vmmap_write(), EFAULT\n");
         return 0;
         /*NOT_YET_IMPLEMENTED("VM: vmmap_write");
         return 0;*/
